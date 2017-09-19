1847b21fed4255c017b4980e51056e4916382c9e
Lukasz Miroslaw <lukmiros@gmail.com>
2017-09-19 15:59:13 +0200

    Update run_job.sh

--- Content/Azure Batch Masterclass Labs/run_job.sh
+++ Content/Azure Batch Masterclass Labs/run_job.sh
@@ -3,9 +3,9 @@
 JOBID=jobtest
 POOLID=ilastik
 
-az batch job create --id $JOBID --pool-id $POOLID --account-endpoint https://matlabb.westeurope.batch.azure.com --account-name matlabb # 
+az batch job create --id $JOBID --pool-id $POOLID --account-endpoint https://ilastikb.westeurope.batch.azure.com --account-name ilastikb # 
 for k in {1..2} 
   do 
     echo "starting task_$k ..."
-    az batch task create --job-id $JOBID --task-id "task_$k" --command-line "/mnt/batch/tasks/shared/run_task.sh $k > out.log" --account-endpoint https://matlabb.westeurope.batch.azure.com --account-name matlabb
+    az batch task create --job-id $JOBID --task-id "task_$k" --command-line "/mnt/batch/tasks/shared/run_task.sh $k > out.log" --account-endpoint https://ilastikb.westeurope.batch.azure.com --account-name ilastikb
   done
